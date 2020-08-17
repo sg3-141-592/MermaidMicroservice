a0be1097b237b8ba39a7ca3d9483553871c44c5f
Sean Dev <5122866+sg3-141-592@users.noreply.github.com>
2020-08-17 17:50:24 +0100

    Working baseline

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const child_process = require('child_process');
 
 const app = express()
 
-app.get('/render', (req, res) => {
-    res.send('Hi');
+app.use(express.json());
+
+app.post('/render', (req, res) => {
+    var requestId = uuidv4();
+
+    mermaidData = req.body.data;
+
+    try {
+        fs.writeFileSync(`inputs/${requestId}`, mermaidData);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+
+    try {
+        child_process.execSync(`mmdc -i inputs/${requestId} -o outputs/${requestId}.png`);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send(error.message);
+    }
+    
+    res.send(`outputs/${requestId}.png`);
 });
 
 app.use(express.static('static'));
+app.use('/outputs', express.static('outputs'));
 
 app.listen(3000, () => {
     console.log("Server listening")
