ee89eef688e01b702c2f644bcfa074c23920024d
Dishy.Dev <dishy@dev>
2020-08-21 17:33:50 +0000

    Added support for docker

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/render', (req, res) => {
     }
 
     try {
-        child_process.execSync(`mmdc -i inputs/${requestId} -o outputs/${requestId}.png`);
+        child_process.execSync(`mmdc -i inputs/${requestId} -o outputs/${requestId}.png -p puppeteer-config.json`);
     } catch (error) {
         console.log(error.message);
         res.status(500).send(error.message);
